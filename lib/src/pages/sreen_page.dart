86e2f6fa466b9a6e2efa78034d3cc5a8b68209c5
Soul Reaper <luisanthonydiegohuamanpaucar@gmail.com>
2020-12-04 09:20:39 -0500

    Se agrego estilos a los texto y al boton

--- lib/src/pages/sreen_page.dart
+++ lib/src/pages/sreen_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ScreenPage extends StatefulWidget {
   ScreenPage({Key key}) : super(key: key);
 
@@ -19,22 +18,39 @@ class _ScreenPageState extends State<ScreenPage> {
             child: Column(
               children: [
                 Container(
-                  margin: const EdgeInsets.only(
-                    top: 100
-                  ),
-                  child: Text('Login')),
-                Text('TextoCualquiera')
+                    margin: const EdgeInsets.only(top: 100),
+                    child: Text('GUSTÓ & MÁS',
+                        style:
+                            TextStyle(color: Colors.blueAccent, fontSize: 40))),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                      'La cocina de autor es poner tu personalidad en lo que haces y ese sentimiento la convierte en algo distinto',
+                      style: 
+                            TextStyle(color: Colors.purpleAccent, fontSize: 15)))
               ],
             ),
           ),
           Container(
-            child: Center(
-              child: Image.asset('assets/images/kindred.png')
-            ),
+            child: Center(child: Image.asset('assets/images/kindred.png')),
           ),
           Container(
-            child: Center(
-              child: Text('button'),
+            child: GestureDetector(
+              onTap: () {
+              },
+              child: Container(
+                margin: EdgeInsets.all(12),
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Center(
+                  child: Text('Iniciar Sesión',
+                      style: TextStyle(color: Colors.white, fontSize: 25)),
+                ),
+              ),
             ),
           ),
         ],
